aafc96a363aabe761616f8c6172c249420723181
Brenda Martinez <bvergaram1500@alumno.ipn.mx>
2021-04-21 20:04:30 -0500

    Lista completamente funcional, sin agregar fecha.

--- main.js
+++ main.js
@@ -9,10 +9,16 @@ const tareas = []
 //Funciones
 
 const agregarTarea = () => {
-    const nuevaTarea = (materiaTexto.value + " " + tareaTexto.value )
-    if(nuevaTarea && nuevaTarea.length > 0){
-        tareas.push(nuevaTarea)
+    const nuevaTarea = {
+        tarea: materiaTexto.value,
+        materia:tareaTexto.value
     }
+
+
+    if(nuevaTarea.tarea && nuevaTarea.materia && nuevaTarea.tarea.length>0 && nuevaTarea.materia.length>0){
+    tareas.push(nuevaTarea)
+    }
+
     materiaTexto.value = ''
     tareaTexto.value = ''
     console.log(tareas)
@@ -20,15 +26,22 @@ const agregarTarea = () => {
 }
 
 const mostrarTareas = () => {
-    let html = ''
-    tareas.forEach((tarea, id) => {
-        html += generarUnaTarea(tarea, id)
+    let html = '', i=0
+    tareas.forEach((id) => {
+        const valores = Object.values(id)
+        html += generarUnaTarea(valores[1], valores[0], i++)
     })
     lista.innerHTML = html
 }
 
-const generarUnaTarea = (tarea, id) => {
-    return `<li id="t${id}">${tarea}<span>❌ </span></li>`
+const generarUnaTarea = (materia,tarea, id) => {
+    return `<li id="t${id}"> ${materia} ${tarea} <span onclick="eliminarTarea(${id})">❌</span></li>`
+}
+
+const eliminarTarea = (id) => {
+    tareas.splice(id , 1)
+    mostrarTareas()
+
 }
 
 
